84ad72bd8e7cea67cd9a486ce84f05a426933de0
valyastriz <valyastriz@gmail.com>
2024-06-29 12:33:34 -0400

    wrapped questions array in a promptUser function and added readme data to the generateMarkdown function

--- index.js
+++ index.js
@@ -2,74 +2,76 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'Enter your project title: ',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter project description: ',
-    },
-    {
-        type: 'input',
-        name: 'intallation',
-        message: 'Enter installation instructions',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage instructions',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Select license type: ',
-        choices: [
-            { name: 'MIT License', value: 'mit' },
-            { name: 'GNU General Public License (GPL) v3.0', value: 'gpl' },
-            { name: 'Apache License 2.0', value: 'apache' },
-            { name: 'Mozilla Public License 2.0', value: 'mozilla' },
-            { name: 'BSD 3-Clause License', value: 'bsd3' },
-            { name: 'BSD 2-Clause License', value: 'bsd2' },
-            { name: 'Creative Commons Zero v1.0 Universal (CC0)', value: 'cco' },
-            { name: 'Eclipse Public License 2.0', value: 'eclipse' },
-            { name: 'GNU Affero General Public License (AGPL) v3.0', value: 'agpl' },
-            { name: 'GNU Lesser General Public License (LGPL) v3.0', value: 'lgpl' },
-            { name: 'Unlicense', value: 'unlicense' },
-        ],
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Explain how others can contribute to your project: ',
-    },
-    {
-        tests: 'input',
-        name:'tests',
-        message: 'Explain how others can test your project',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter the link to your GitHub repository: ',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email: ',
-    },
-];
-
-inquirer.prompt(questions);
+const promptUser = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter your project title: ',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Enter project description: ',
+            },
+            {
+                type: 'input',
+                name: 'intallation',
+                message: 'Enter installation instructions: ',
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Enter usage instructions: ',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Select license type: ',
+                choices: [
+                    { name: 'MIT License', value: 'mit' },
+                    { name: 'GNU General Public License (GPL) v3.0', value: 'gpl' },
+                    { name: 'Apache License 2.0', value: 'apache' },
+                    { name: 'Mozilla Public License 2.0', value: 'mozilla' },
+                    { name: 'BSD 3-Clause License', value: 'bsd3' },
+                    { name: 'BSD 2-Clause License', value: 'bsd2' },
+                    { name: 'Creative Commons Zero v1.0 Universal (CC0)', value: 'cco' },
+                    { name: 'Eclipse Public License 2.0', value: 'eclipse' },
+                    { name: 'GNU Affero General Public License (AGPL) v3.0', value: 'agpl' },
+                    { name: 'GNU Lesser General Public License (LGPL) v3.0', value: 'lgpl' },
+                    { name: 'Unlicense', value: 'unlicense' },
+                ],
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Explain how others can contribute to your project: ',
+            },
+            {
+                tests: 'input',
+                name:'tests',
+                message: 'Explain how others can test your project: ',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter the link to your GitHub repository: ',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter your email: ',
+            },
+        ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,47 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  WHEN I click on the links in the Table of Contents
+  THEN I am taken to the corresponding section of the README
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ```
+
+  ```
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing
+  ${data.constributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  WHEN I enter my GitHub username
+  THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+  WHEN I enter my email address
+  THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 `;
 }
 
